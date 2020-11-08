5786c798bf0bf1de159f217d6cdb4918bc93e79c
kuuran <felixjacquemin@gmail.com>
2020-11-08 18:46:31 +0100

    ajout de la classe Point

---
+++ Point.java
@@ -0,0 +1,20 @@
+public class Point {
+    private int nb;
+    private float x, y;
+
+    public Point(int n, float x, float y){
+        nb = n;
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * calcule la distance entre deux points
+     * @param p
+     * @return distance entre le point courant et le parametre p
+     */
+    public float distance (Point p){
+        return 0;
+    }
+
+}
